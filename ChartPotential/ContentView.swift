444aa9c65404159e82b8b187bf8a9b458d0d0205
Darren Li <68171190+meow-fish@users.noreply.github.com>
2024-01-27 22:33:28 +0800

    Update ContentView.swift
    I add new description of my code

--- ChartPotential/ContentView.swift
+++ ChartPotential/ContentView.swift
@@ -1,17 +1,16 @@
-//
 //  ContentView.swift
-//  PotentialScore
-//
 //  Created by Darren Li on 25/1/2024.
 //
+//  "ptt" means potential
 
 import SwiftUI
 import Foundation
 import Darwin
 
+//The initial page
 struct InputView: View {
-    @State private var potential = ""
-    @State private var constant = ""
+    @State private var potential = "" //target chart potential
+    @State private var constant = "" //chart constant
     var body: some View {
         NavigationStack{
             VStack (spacing:15){
@@ -35,27 +34,28 @@ struct InputView: View {
         }
     }
 
+//The answer page
 struct ShowView: View {
-    var constant = ""
-    var potential = ""
+    var constant = ""  //For receiving input chart constant from the previous page
+    var potential = "" //For receiving input target chart potential from the previous page
     func FindTargetScore(A:String, B:String) -> String{
-        var const = Double(A) ?? 0.0
-        var poten = Double(B) ?? 0.0
+        var const = Double(A) ?? 0.0 //convert String-type chart constant to Double
+        var poten = Double(B) ?? 0.0 //convert String-type potential to Double
         var chart_ptt:Double
         var target = ""
-        if const - poten < 2{
-            for i in 1...500001{
-                if i < 300000{
+        if const - poten < 2{ //If difference > 2, it is impossible to reach the target chart potential
+            for i in 1...500001{ //Check the possible score to reach target chart potential from 950w to 1000w
+                if i < 300000{ //Use the rule for calculating chart ptt from 950w to 980w
                     chart_ptt = const+(Double(i)/300000.0)
-                    if chart_ptt > poten{
+                    if chart_ptt > poten{ //To make sure the chart ptt is higher than the target chart ptt
                         target = String(i+9500000)
                         break
                     }
                     var i = i + 1
                 }
-                if i > 300000 {
+                if i > 300000 { //Use the rule for calculating chart ptt from 980w to 1000w
                     chart_ptt = const+1+((Double(i)-300000)/200000.0)
-                    if chart_ptt > poten{
+                    if chart_ptt > poten{ //To make sure the chart ptt is higher than the target chart ptt
                         target = String(i-300000+9800000)
                         break
                     }
@@ -67,6 +67,6 @@ struct ShowView: View {
     }
     
     var body: some View {
-        Text(FindTargetScore(A:constant, B:potential))
+        Text(FindTargetScore(A:constant, B:potential)) //Show the possible score
     }
 }
